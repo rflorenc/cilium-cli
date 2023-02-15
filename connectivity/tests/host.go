df5707393d53ff8c4f1b0bca60687f9cdd4128e6
Martynas Pumputis <m@lambda.lt>
2023-02-15 16:30:09 +0100

    WIP
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- connectivity/tests/host.go
+++ connectivity/tests/host.go
@@ -26,33 +26,33 @@ func (s *podToHost) Name() string {
 func (s *podToHost) Run(ctx context.Context, t *check.Test) {
 	ct := t.Context()
 	// Construct a unique list of all nodes in the cluster running workloads.
-	// TODO(timo): Should probably use Cilium agent Pods or actual nodes as the
-	// source of truth here.
-	nodes := make(map[string]check.TestPeer)
-	for _, client := range ct.ClientPods() {
-		ip := client.Pod.Status.HostIP
-		nodes[ip] = check.ICMPEndpoint("", ip)
-	}
-	for _, echo := range ct.EchoPods() {
-		ip := echo.Pod.Status.HostIP
-		nodes[ip] = check.ICMPEndpoint("", ip)
-	}
 
 	var i int
 
 	for _, pod := range ct.ClientPods() {
 		pod := pod // copy to avoid memory aliasing when using reference
 
-		for _, node := range nodes {
-			t.NewAction(s, fmt.Sprintf("ping-%d", i), &pod, node, check.IPFamilyTODO).Run(func(a *check.Action) {
-				a.ExecInPod(ctx, ct.PingCommand(node, check.IPFamilyTODO))
+		for _, node := range ct.Nodes() {
+			node := node
 
-				a.ValidateFlows(ctx, pod, a.GetEgressRequirements(check.FlowParameters{
-					Protocol: check.ICMP,
-				}))
-			})
+			for _, addr := range node.Status.Addresses {
+				if !t.Context().IsIPAddrFamilySupported(addr.Address) {
+					continue
+				}
 
-			i++
+				dst := check.ICMPEndpoint("", addr.Address)
+				ipFam := check.GetIPFamily(addr.Address)
+
+				t.NewAction(s, fmt.Sprintf("ping-%d", i), &pod, dst, ipFam).Run(func(a *check.Action) {
+					a.ExecInPod(ctx, ct.PingCommand(dst, ipFam))
+
+					a.ValidateFlows(ctx, pod, a.GetEgressRequirements(check.FlowParameters{
+						Protocol: check.ICMP,
+					}))
+				})
+
+				i++
+			}
 		}
 	}
 }
